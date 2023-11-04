c353b7a6296099d19ab21a940ac064821a674de2
Vixen-1 <ayushisaxena24111999@gmail.com>
2023-11-04 21:10:46 +0530

    till auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const User = require("../models/User");
+
+//to create routes on the link we use router 
 const router = express.Router();
+
+//we use express validation so that we can ensure if email is correct as per our criteria
 const { body, validationResult } = require("express-validator");
+
+//we use bcrypt so that we can generate hashcodes for password which can get saved in database.
+const bcrypt = require("bcryptjs");
+
+//userid password create krne ke baad we will give a token to the user so that he can login successfully. using jsonwebtoken i.e. jwt.io
+// jwt client and server ke bichme secure communication ensure krega.
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = "Ayuisagoodg$irl"
 
 //create a user using: POST "api/auth/createuser". Does'nt require login
 router.post(
@@ -9,9 +21,7 @@ router.post(
   [
     body("name", "Name must have minimum 3 characters").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
-    body("password", "password must contain minimum 5 characters").isLength({
-      min: 5,
-    }),
+    body("password", "password must contain minimum 5 characters").isLength({min: 5}),
   ],
   async (req, res) => {
     // if there are errors return bad request and the errors
@@ -28,13 +38,22 @@ router.post(
           .json({ error: "sorry a user with this email already exist." });
       }
       //create a new user
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
 
-      res.json(user);
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // console.log(jwtData);
+      res.json({authToken});
     } 
     //catch errors
     catch (error) {
